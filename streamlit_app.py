9738fe452e8068685bff873ed483de41e8631d1c
kolpitor <65658085+kolpitor@users.noreply.github.com>
2022-03-21 16:31:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ os.system('sh build.sh')
 os.system('cd python-package')
 os.system('python setup.py install')
 os.system('pip install graphviz')
-os.system('conda install scikit-learn')
+os.system('pip3 install -U scikit-learn scipy matplotlib')
 
 from collections import namedtuple
 import altair as alt
