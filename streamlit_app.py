30479afceff219af3e8cc3209948b8defff119b5
kolpitor <65658085+kolpitor@users.noreply.github.com>
2022-03-21 17:48:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ os.system('sh build.sh')
 os.system('cd python-package')
 os.system('python setup.py install')
 os.system('pip install graphviz')
-os.system('pip install python-pydot python-pydot-ng')
+os.system('pip install python-pydot')
+os.system('pip install python-pydot-ng')
 os.system('pip install -U scikit-learn scipy matplotlib')
 python-pydot python-pydot-ng
 from collections import namedtuple
