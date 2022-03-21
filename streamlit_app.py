6a78e4cfac1e780082331d2a0aa2704e76936fa7
kolpitor <65658085+kolpitor@users.noreply.github.com>
2022-03-21 17:01:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,3 @@
-import os
-os.system('git clone --recursive https://github.com/dmlc/xgboost')
-os.system('cd xgboost')
-os.system('sudo cp make/minimum.mk ./config.mk;')
-os.system('sudo make -j4;')
-os.system('sh build.sh')
-os.system('cd python-package')
-os.system('python setup.py install')
-os.system('pip install graphviz')
-os.system('pip install -U scikit-learn scipy matplotlib')
-
 from collections import namedtuple
 import altair as alt
 import math
@@ -16,6 +5,7 @@ import streamlit as st
 import pandas
 import numpy
 import xgboost
+import graphviz
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot
