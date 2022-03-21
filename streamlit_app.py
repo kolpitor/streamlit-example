5d49afe6c793a97e3cad9bf6b1682b144b18cf3d
kolpitor <65658085+kolpitor@users.noreply.github.com>
2022-03-21 17:10:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import os
+os.system('git clone --recursive https://github.com/dmlc/xgboost')
+os.system('cd xgboost')
+os.system('sudo cp make/minimum.mk ./config.mk;')
+os.system('sudo make -j4;')
+os.system('sh build.sh')
+os.system('cd python-package')
+os.system('python setup.py install')
 os.system('pip install graphviz')
+os.system('pip install -U scikit-learn scipy matplotlib')
 
 from collections import namedtuple
 import altair as alt
